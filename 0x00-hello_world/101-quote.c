8325c4d65454c40c2808c7550b94282785c486e6
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-16 12:14:23 +0100

    fixing str namex

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
-	fprintf(stderr, "%s", string_to_print);
+	fprintf(stderr, "%s", str);
 
 	return (1);
 }
