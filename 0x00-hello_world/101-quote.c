9daec79121f0b45996e93c804deb3fc4bf90f73e
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-16 12:05:53 +0100

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: entry point of the program
+ *
+ * Return: 0 if seccussful
+ */
+
+int main(void) /* The main function is the strating point of the program */
+{
+	string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%s", string);
+
+	return (1);
+}
