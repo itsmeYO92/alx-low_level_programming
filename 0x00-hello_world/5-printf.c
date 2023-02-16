5bf401c68662fa421df7c2d3663145a6e452889d
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-16 11:30:50 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: entry point of the program
+ *
+ * Return: 0 if seccussful
+ */
+
+int main(void) /* The main function is the strating point of the program */
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
