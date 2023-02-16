47456a8088aa0a03590209a83bde79f19e439c39
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-16 11:32:36 +0100

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,7 +10,7 @@
 
 int main(void) /* The main function is the strating point of the program */
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
