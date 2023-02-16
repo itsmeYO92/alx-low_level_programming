3b3893600d7cad26b8bdf6cd460de7ccd5cb0888
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-16 11:41:40 +0100

    changing puts to printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@
 
 int main(void) /* The main function is the strating point of the program */
 {
-	puts("Size of a char: %d byte(s)\n",sizeof(char));
-	puts("Size of an int: %d byte(s)\n",sizeof(int));
-	puts("Size of a long int: %d byte(s)\n",sizeof(long int));
-	puts("Size of a long long int: %d byte(s)\n",sizeof(long long int));
-	puts("Size of a float: %d byte(s)\n",sizeof(float));
+	printf("Size of a char: %d byte(s)\n",sizeof(char));
+	printf("Size of an int: %d byte(s)\n",sizeof(int));
+	printf("Size of a long int: %d byte(s)\n",sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n",sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n",sizeof(float));
 
 	return (0);
 }
