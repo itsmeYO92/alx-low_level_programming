c440e22d24345c4e8e48114f856699488b503df6
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-17 12:28:43 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers from 10 to 9.
+ *
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
