35d95c472964026bd03979e1f4d53262c0204033
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-17 12:39:43 +0100

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print the alphabets to the standard output using only putchar
+ *
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i < '9' + 1; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'a'; i < 'z' + 1; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
