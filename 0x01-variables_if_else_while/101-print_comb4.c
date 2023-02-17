ef057828baaf7c0256774f96fcf2aee9cf2086c0
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-17 13:23:16 +0100

    advanced task 1

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers from 10 to 9.
+ *
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	int i;
+	int x;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			for (x = j + 1; x < 58; x++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(x);
+				if (i != 56)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
