aa504ccb8021d02a02de62b3d77029fde8a5543f
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-17 12:12:21 +0100

    fixing betty errors

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: Print the alphabets to the standard output using only putchar 
+ * Description: Print the alphabets to the standard output using only putchar
  *
  * Return: 0 if successful
 */
 int main(void)
 {
-	for (char i='a'; i < 'z'+1; i++)
+	for (char i = 'a'; i < 'z' + 1; i++)
 	{
 		putchar(i);
 	}
