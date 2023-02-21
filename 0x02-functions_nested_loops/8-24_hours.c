2976b885aa10530abf7237c663a0091e77d2ce37
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-21 21:31:37 +0100

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * jack_bauer  - Entry point
+ *
+ * Description: print every minute of the day
+ *
+ * Return: 0 if seccessful
+*/
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60 ; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar('\n');
+
+	return (0);
+
+}
+
