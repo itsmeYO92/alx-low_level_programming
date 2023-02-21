b5db7225c82745b69b37b83ec5532de34e864eca
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-21 12:18:12 +0100

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print _putchar
+ *
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
