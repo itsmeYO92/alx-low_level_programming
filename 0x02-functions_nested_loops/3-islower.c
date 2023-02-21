ae1324fd83471a151c9e9fd73d43043b617b1c77
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-21 15:12:07 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Description: check if char c is lowercase
+ *
+ * Return: 1 if lower 0 if not 
+*/
+int _islower(int c)
+{
+
+	if ('a' <= c <= 'z')
+	{
+		return (1);
+	}
+	else
+	}
+		return (0);
+	}
+
+}
+
