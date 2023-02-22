6ed5ebf30f1ac2b6730cf557ac26c13fae94ffaf
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-22 19:29:08 +0100

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: print sum of multiples of 3 and 5
+ *
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	char i, res = 0;
+
+	for (i = 2; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			res += i;
+	}
+	_putchar(res + '0');
+	_putchar('\n');
+	return (0);
+}
+
