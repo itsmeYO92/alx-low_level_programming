801faa5ec0cc843ac0021a6949eab9dcffc0c696
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-23 12:08:37 +0100

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a:first number
+ * @b: second number
+ * Description: multiplication of two numbers
+ *
+ * Return: a * b
+*/
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 int _isupper(char c);
 int _isdigit(int c);
-
+int mul(int a, int b);
 
 #endif
