90facc1a47f4125be2aa4eca6201aef1aacb980d
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-23 18:06:39 +0100

    fixing errors

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 {
 	int i = 10000;
 
-	if (j < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
@@ -25,5 +25,5 @@ void print_number(int n)
 		i = i / 10;
 	}
 	_putchar(n % 10 + '0');
-	_putchar('\n');
+
 }
