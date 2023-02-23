e60db6b4ee7202ae7cb471a44a2a6c01295ad4bb
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-23 12:29:50 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ * Description: print numbers from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i / 10 != 0)
+		{
+			_putchar(i / 10 + '0');
+		}
+		_putchar(i % 10 + '0');
+	}
+	_putchar('\n');
+
+}
+
