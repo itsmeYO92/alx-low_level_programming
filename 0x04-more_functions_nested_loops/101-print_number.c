c7df487f1c8310a27b3e1205b58d5f56fb581075
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-23 18:20:52 +0100

    using reccursive functions

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,22 +8,14 @@
  */
 void print_number(int n)
 {
-	int i = 1000000000;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	while (i > 1)
-	{
-		if (n / i != 0)
-		{
-			_putchar(n / i + '0');
-			n = n - (n / i) * i;
-		}
-		i = i / 10;
-	}
+	if (n / 10 != 0)
+		print_number(n / 10);
 	_putchar(n % 10 + '0');
 
 }
