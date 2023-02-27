e3d8c663d984bba323c03f7215397088b66ea34b
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-27 13:25:01 +0100

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -19,13 +19,12 @@ void print_array(int *a, int n)
 	{
 		if (i != n - 1)
 		{
-			printf("%d ,", *(a + i));
+			printf("%d, ", *(a + i));
 		}
 		else
 		{
-			printf("%d", *(a + i));
+			printf("%d\n", *(a + i));
 		}
 	}
-	putchar('\n');
 
 }
