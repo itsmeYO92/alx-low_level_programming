c62715e2a08ad7ca861401e367ba472bdea22885
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-27 10:48:27 +0100

    changing value with pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - change the value of n to 98
+ *
+ * @n: pointer to the value
+ *
+ * return: nothing
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
