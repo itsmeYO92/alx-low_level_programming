dd7ced0a4d12e054cef1d93fe91c7283877a343b
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-02-27 11:26:19 +0100

    print a string in rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print a string in reverse
+ *
+ * @s: string to print
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int length, i;
+
+	length = strlen(s);
+	for (i = length - 1; i >= 0; i--)
+		_putchar(*(s + i);
+	_putchar('\n');
+
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 void reset_to_98(int *n);
+void print_rev(char *s);
 void _putchar(char c);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
