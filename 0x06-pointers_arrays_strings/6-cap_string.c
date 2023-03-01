dc13eb4e2c25c5e03d6c01fee6886dfd88325b08
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-01 14:12:34 +0100

    caps

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - capitalise every word in string
+ *
+ * @s: pointer to string
+ *
+ * Return: pointer to string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int stat  = 1;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (stat == 1)
+			{
+				*(s + i) -= 32;
+				stat = 0;
+			}
+		}
+		else if (*(s + i) <= 'A' || *(s + i) >= 'Z')
+			stat = 1;
+		else
+			stat = 0;
+		i++;
+	}
+	return (s);
+
+}
