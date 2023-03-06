bbeaa133f7497635adb5572ff84ad75db6171028
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-06 22:07:55 +0100

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * set_string - count number of occurence of chars in a string
+ *
+ * @s: point to pointer to a string
+ * @to: stirng of accepted chars
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
