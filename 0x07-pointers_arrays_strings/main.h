e0e5ab20f28548a957757f5dda2dbc082ce2cceb
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-06 22:09:43 +0100

    updating main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 char *_memset(char *s, char b, unsigned int n);
+void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
