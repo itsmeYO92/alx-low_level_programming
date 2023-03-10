5156c4d65378a4139fd11e3637cd8faf9af0c082
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-10 12:53:04 +0100

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print program name
+ *
+ * @argc: number of parametres
+ *
+ * @argv: array with parameters
+ *
+ * Return: 0 if successful
+*/
+int main(int argc, char **argv)
+{
+	int a;
+	int b;
+
+	if (argc < 3)
+	{
+		printf("Error");
+		return (1);
+	}
+
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	printf("%d\n", a * b);
+
+	return (0);
+
+}
+
