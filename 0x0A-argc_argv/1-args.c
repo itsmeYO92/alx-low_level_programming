e6525e7f738b11be31a34f45b4126e4a06e2f45a
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-10 12:39:46 +0100

    printing args number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print program name
+ *
+ * @argc: number of parametres
+ *
+ * @argv: array with parameters
+ *
+ * Return: 0 if successful
+*/
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+
+}
+
