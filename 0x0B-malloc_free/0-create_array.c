a8938b9825041368bf4fde9c34cba6081de35bff
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-13 21:42:21 +0100

    check if p is null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	p = (char *)malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		*(p + i) = c;
 	return (p);
