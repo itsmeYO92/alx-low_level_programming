810522db797a137ca193a7864e631c70b7bbcd49
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-13 12:32:08 +0100

    initialising

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	p = (*int)malloc(sizeof(char) * size);
+	p = malloc(sizeof(char) * size);
 	for (int i = 0; i < n; i++)
 		*(p + i) = c;
 	return (p);
