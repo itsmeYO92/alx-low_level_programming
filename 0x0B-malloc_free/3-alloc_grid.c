59d17f2d98a6a5f130e6e72a14222e2568917af4
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-14 21:45:30 +0100

    free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,14 +17,20 @@ int **alloc_grid(int width, int height)
 
 	p = (int **) malloc(sizeof(int) * height);
 	if (p == NULL)
+	{
+		free(p);
 		return (NULL);
-
+	}
 	for (i = 0; i < height; i++)
 	{
 		p[i] = (int *)malloc(sizeof(int) * width);
 		if (p[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(p[i]);
+			free(p);
 			return (NULL);
-
+		}
 		for (j = 0; j < width; j++)
 			p[i][j] = 0;
 	}
