368ff9d1cbef536eba045ea07a8542420b04ad0c
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-14 20:23:19 +0100

    memory allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	p = (char *)malloc(sizeof(char) * strlen(str));
+	p = (char *)malloc(1 + sizeof(char) * strlen(str));
 	if (p == NULL)
 		return (NULL);
 
