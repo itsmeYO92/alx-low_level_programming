23fccb198182e0be0fd125103c876c1d10a33030
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-16 12:26:37 +0100

    array of int

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - creat array of int
+ * @min: first int
+ * @max: last int
+ * Return: pointer to new allocated memory
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	unsigned int i, l;
+
+	if (max - min < 0)
+		return (NULL);
+
+	l = max - min + 1;
+
+	p = malloc(l * sizeof(int);
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < l; i++)
+		*(p + i) = min + i;
+
+	return (p);
+}
