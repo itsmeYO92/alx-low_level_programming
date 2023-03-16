7b8a9a9645514653628df4f09d5d3b3aa739ed05
ayoub aboutarbouch <ay.aboutarbouch@gmail.com>
2023-03-16 16:09:15 +0100

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
  * @new_size: size of the new array
  * Return: pointer to new allocated memory
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr != NULL)
 	{
