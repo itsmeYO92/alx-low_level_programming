d2d6bcc27007ce4ef70594b3276fcf074d43a974
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-03-20 15:43:14 +0000

    print class dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print a dog class
+ *
+ * @d: structure to print
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		if (d->age)
+			printf("Age: %.6f\n", d->age);
+		else
+			printf("Age: (nil)\n");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
