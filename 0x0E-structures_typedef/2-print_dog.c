0cc0c88ed7213ecb47f27a4f9a81d1682a22dbe8
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-03-21 21:08:35 +0000

    fixing one test case

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		if (d->age)
 			printf("Age: %.6f\n", d->age);
 		else
-			printf("Age: (nil)\n");
+			printf("Age: %.6f\n", 0);
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
