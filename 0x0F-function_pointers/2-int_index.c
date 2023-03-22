2335a7ad0d2f5903c811ed4afc0ada89b92f44da
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-03-22 14:13:08 +0000

    check array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	if (cmp)
+	if (cmp && array)
 		for (i = 0; i < size; i++)
 			if (cmp(array[i]) != 0)
 				return (i);
