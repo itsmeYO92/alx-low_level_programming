46f58c235e706d6001685c014ffb8435a666a016
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-03-23 20:36:11 +0000

    betty doc

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,21 +4,38 @@
 #include <string.h>
 
 
+/**
+ * p_int - print int
+ * @arg: int to print
+*/
+
 void p_int(va_list arg)
 {
 	int i = va_arg(arg, int);
+
 	printf("%d", i);
 }
 
+
+/**
+ * p_float - print float
+ * @arg: floaot to print
+*/
 void p_float(va_list arg)
 {
 	float i = va_arg(arg, double);
+
 	printf("%f", i);
 }
 
+/**
+ * p_string - print int
+ * @arg: string to print
+*/
 void p_string(va_list arg)
 {
 	char *str = va_arg(arg, char *);
+
 	if (!str)
 	{
 		printf("(nil)");
@@ -27,6 +44,10 @@ void p_string(va_list arg)
 	printf("%s", str);
 }
 
+/**
+ * p_char - print char
+ * @arg: char to print
+*/
 void p_char(va_list arg)
 {
 	printf("%c", va_arg(arg, int));
@@ -60,7 +81,7 @@ void print_all(const char * const format, ...)
 		{
 			if (*(ops[i].op) == format[j])
 			{
-				printf("%s",delimetre);
+				printf("%s", delimetre);
 				ops[i].f(pars);
 				delimetre = ", ";
 			}
