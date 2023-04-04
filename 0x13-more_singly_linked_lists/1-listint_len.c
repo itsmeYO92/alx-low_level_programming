bea5afaa121b5ee78f7aad831b73c2cd6d3b88b9
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-04-04 10:37:42 +0000

    listint length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,17 @@
 #include "lists.h"
 
 /**
- * print_listint - print asingle linked list
+ * listint_len - count asingle linked list
  * @h: pointer to the head of the single linked list
  * Return: number of nodes
 */
-
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	listint_t *current_node = (listint_t *)h;
-	int count = 0;
+	size_t count = 0;
 
 	while (current_node)
 	{
-		printf("%d\n", current_node->n);
 		current_node = current_node->next;
 		count++;
 	}
