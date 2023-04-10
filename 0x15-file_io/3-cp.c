7ead5b9d083b99dd2755b25adfb295fc176cd858
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-04-10 22:41:50 +0000

    error handling

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,8 +29,8 @@ int main(int ac, char **av)
 	to_file = open(av[2], to_flags, mode);
 	if (to_file == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
+		dprintf(2, "Error: Can't read from file %s\n", av[2]);
+		exit(98);
 	}
 	while (nread != 0 && nread != -1)
 	{
