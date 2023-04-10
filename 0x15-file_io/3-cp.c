f763ba0a0f67caff5522dbff492f73fc8d5f2487
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-04-10 12:11:49 +0000

    closing error printed value

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,9 +43,10 @@ int main(int ac, char **av)
 	}
 	fdf = close(from_file);
 	fdt = close(to_file);
-/*
- *	if (fdt + fdf != 0)
- *		dprintf(2, "Error: Can't close fd %d\n", fdt == -1 ? fdt : fdf);
-*/
+	if (fdt + fdf != 0)
+	{
+	dprintf(2, "Error: Can't close fd %d\n", fdt == -1 ? to_file : from_file);
+		exit(100);
+	}
 	return (0);
 }
