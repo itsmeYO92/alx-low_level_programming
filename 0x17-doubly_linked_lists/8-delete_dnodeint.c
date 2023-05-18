b7100c60436bc957fddaa15c552415e87d2613c5
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-05-18 17:39:14 +0100

    memleaks

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,9 +1,12 @@
 #include "lists.h"
 
 /**
- * free_dlistint - free a doubly linked list.
+ * delete_dnodeint_at_index - Deletes a node at a given index in a doubly linked list.
  *
  * @head: A double pointer to the head of the doubly linked list.
+ * @index: The index of the node to be deleted.
+ *
+ * Return: 1 if the deletion is successful, -1 if it fails.
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -14,7 +17,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		current = (*head)->next;
+		free(*head);
+		if (current)
+			current->prev = NULL;
+		*head = current;
 		return (1);
 	}
 
@@ -24,10 +31,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (current->next)
 			current = current->next;
 		else
+		{
+			free(current);
 			return (-1);
+		}
 	}
 	current->prev->next = current->next;
 	if (current->next)
 		current->next->prev = current->prev;
+	free(current);
 	return (1);
 }
