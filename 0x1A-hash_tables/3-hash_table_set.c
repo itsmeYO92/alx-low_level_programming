4cf81e04d5a1c54993ed0b990cd30269f5f53e55
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-08 20:29:23 +0100

    accessing size before cheking if ht is null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,11 +10,12 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 	hash_node_t *current;
 
 	if (!ht || !key || !value || !(*key))
 		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 	while (current)
 	{
--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -12,8 +12,12 @@ int main(void)
 {
     hash_table_t *ht;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "hetairas", "cool");
-    hash_table_set(ht, "hetairas", "mentioner");
+    //ht = hash_table_create(1024);
+    ht = NULL;
+    int a;
+    a = hash_table_set(ht, "hetairas", "cool");
+    int b;
+    b = hash_table_set(ht, "heas", "");
+    printf("%d ----- %d", a, b);
     return (EXIT_SUCCESS);
 }
