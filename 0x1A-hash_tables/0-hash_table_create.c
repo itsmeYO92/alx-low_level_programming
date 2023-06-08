991f2102101a99728c8782935f06ba5ca7aa4180
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-08 18:39:41 +0100

    free

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,6 +17,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_hash_table->size = size;
 	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new_hash_table->array)
+	{
+		free(new_hash_table);
 		return (NULL);
+	}
 	return (new_hash_table);
 }
