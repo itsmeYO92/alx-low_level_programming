965b0c9bc81b58f42fee4c79c196ab232d5e0b19
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-08 18:32:23 +0100

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: size of the hash table to be created
+ * Return: adress to the new created hash table or NULL if FAILED
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table = malloc(sizeof(hash_node_t));
+
+	if (!new_hash_table)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	new_hash_table->size = size;
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_hash_table->array)
+		return (NULL);
+	return (new_hash_table);
+}
