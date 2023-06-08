bb6c5f990b3b4458bac09d8b5b70c51e58c8f9e3
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-08 20:53:12 +0100

    strcmp error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-/*	hash_node_t *current;*/
+	hash_node_t *current;
 
 	if (!ht || !key || !value || !(*key))
 		return (0);
