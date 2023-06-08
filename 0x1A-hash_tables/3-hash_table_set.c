571482643f817082a909ab9b6495276978856cf2
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-06-08 20:31:14 +0100

    debug

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,6 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || !value || !(*key))
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
+/*
 	current = ht->array[index];
 	while (current)
 	{
@@ -27,6 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
+*/
 	return (add_node(&(ht->array[index]), value, key));
 }
 
