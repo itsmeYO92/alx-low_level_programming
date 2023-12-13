eb0a482030464d4792f4817003c0acfb24a455a2
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-12-13 18:17:01 +0100

    advanced binary search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,61 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * advanced_binary - a function to search an array using binary search
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index of the value or -1 if not found
+*/
+int advanced_binary(int *array, size_t size, int value)
+{
+	int low, mid, high;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		print_array(array, low, high);
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+		{
+			while (mid > 0 && array[mid - 1] == value)
+				mid--;
+			return (mid);
+		}
+	}
+	return (-1);
+}
+
+
+
+/**
+ * print_array - a function to print the array to search
+ * @array: array to print
+ * @size: size of the array
+ * @start: index to start from
+*/
+
+void print_array(int *array, size_t start, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i < size; i++)
+	{
+		printf("%d, ", *(array + i));
+	}
+	printf("%d\n", *(array + size));
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t size);
 int jump_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
